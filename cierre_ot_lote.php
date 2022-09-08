edf3440e17ad2d60e35f85eae0916c4bbd5340e1
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2022-09-08 14:10:59 -0500

    primer commit cierre ot

--- cierre_ot_lote.php
+++ cierre_ot_lote.php
@@ -1,66 +1,66 @@
-<?php require_once("includes/conexion.php");
+<?php require_once "includes/conexion.php";
 PermitirAcceso(311);
 
-$sw=0;
+$sw = 0;
 
 //Estado llamada
-$SQL_EstadoLlamada=Seleccionar('uvw_tbl_EstadoLlamada','*');
+$SQL_EstadoLlamada = Seleccionar('uvw_tbl_EstadoLlamada', '*');
 
 //Sucursal
-$ParamSucursal=array(
-	"'".$_SESSION['CodUser']."'"
+$ParamSucursal = array(
+    "'" . $_SESSION['CodUser'] . "'",
 );
-$SQL_Sucursal=EjecutarSP('sp_ConsultarSucursalesUsuario',$ParamSucursal);
+$SQL_Sucursal = EjecutarSP('sp_ConsultarSucursalesUsuario', $ParamSucursal);
 
-if(isset($_GET['Sucursal'])&&$_GET['Sucursal']!=""){
-	//Serie de llamada
-	$ParamSerie=array(
-		"'".$_SESSION['CodUser']."'",
-		"'191'"
-	);
-	$SQL_Series=EjecutarSP('sp_ConsultarSeriesDocumentos',$ParamSerie);
+if (isset($_GET['Sucursal']) && $_GET['Sucursal'] != "") {
+    //Serie de llamada
+    $ParamSerie = array(
+        "'" . $_SESSION['CodUser'] . "'",
+        "'191'",
+    );
+    $SQL_Series = EjecutarSP('sp_ConsultarSeriesDocumentos', $ParamSerie);
 }
 
 //Fechas
-if(isset($_GET['FechaInicial'])&&$_GET['FechaInicial']!=""){
-	$FechaInicial=$_GET['FechaInicial'];
-	$sw=1;
-}else{
-	//Restar 7 dias a la fecha actual
-	$fecha = date('Y-m-d');
-	$nuevafecha = strtotime ('-'.ObtenerVariable("DiasRangoFechasDocSAP").' day');
-	$nuevafecha = date ( 'Y-m-d' , $nuevafecha);
-	$FechaInicial=$nuevafecha;
+if (isset($_GET['FechaInicial']) && $_GET['FechaInicial'] != "") {
+    $FechaInicial = $_GET['FechaInicial'];
+    $sw = 1;
+} else {
+    //Restar 7 dias a la fecha actual
+    $fecha = date('Y-m-d');
+    $nuevafecha = strtotime('-' . ObtenerVariable("DiasRangoFechasDocSAP") . ' day');
+    $nuevafecha = date('Y-m-d', $nuevafecha);
+    $FechaInicial = $nuevafecha;
 }
-if(isset($_GET['FechaFinal'])&&$_GET['FechaFinal']!=""){
-	$FechaFinal=$_GET['FechaFinal'];
-	$sw=1;
-}else{
-	$FechaFinal=date('Y-m-d');
+if (isset($_GET['FechaFinal']) && $_GET['FechaFinal'] != "") {
+    $FechaFinal = $_GET['FechaFinal'];
+    $sw = 1;
+} else {
+    $FechaFinal = date('Y-m-d');
 }
 
-$Recargar=isset($_GET['reload']) ? $_GET['reload'] : 0;
+$Recargar = isset($_GET['reload']) ? $_GET['reload'] : 0;
 
-if($sw==1){
-	$Param=array(
-		"'".FormatoFecha($FechaInicial)."'",
-		"'".FormatoFecha($FechaFinal)."'",
-		"'".$_GET['Series']."'",
-		"'".strtolower($_SESSION['User'])."'",
-		"'".$_GET['EstadoLlamada']."'",
-		$Recargar
-	);
-	$SQL=EjecutarSP('usp_tbl_CierreOrdenesServicio_Sel',$Param);
-	$row=sqlsrv_fetch_array($SQL);
+if ($sw == 1) {
+    $Param = array(
+        "'" . FormatoFecha($FechaInicial) . "'",
+        "'" . FormatoFecha($FechaFinal) . "'",
+        "'" . $_GET['Series'] . "'",
+        "'" . strtolower($_SESSION['User']) . "'",
+        "'" . $_GET['EstadoLlamada'] . "'",
+        $Recargar,
+    );
+    $SQL = EjecutarSP('usp_tbl_CierreOrdenesServicio_Sel', $Param);
+    $row = sqlsrv_fetch_array($SQL);
 }
 ?>
 <!DOCTYPE html>
 <html><!-- InstanceBegin template="/Templates/PlantillaPrincipal.dwt.php" codeOutsideHTMLIsLocked="false" -->
 
 <head>
-<?php include("includes/cabecera.php"); ?>
+<?php include "includes/cabecera.php";?>
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Cierre de OT en lote | <?php echo NOMBRE_PORTAL;?></title>
+<title>Cierre de OT en lote | <?php echo NOMBRE_PORTAL; ?></title>
 	<!-- InstanceEndEditable -->
 <!-- InstanceBeginEditable name="head" -->
 <style>
@@ -77,9 +77,9 @@ if($sw==1){
 			var Cliente=document.getElementById("ClienteActividad");
 			if(NomCliente.value==""){
 				Cliente.value="";
-			}	
+			}
 		});
-		
+
 		$("#Sucursal").change(function(){
 			$('.ibox-content').toggleClass('sk-loading',true);
 			var Sucursal=document.getElementById('Sucursal').value;
@@ -90,10 +90,10 @@ if($sw==1){
 					$('#Series').html(response).fadeIn();
 					$('.ibox-content').toggleClass('sk-loading',false);
 				}
-			});	
+			});
 		});
-		
-	});	
+
+	});
 </script>
 <script>
 function Validar(Tipo){
@@ -108,7 +108,7 @@ function Validar(Tipo){
 		if (result.isConfirmed) {
 			EjecutarProceso(Tipo);
 		}
-	});	
+	});
 }
 
 function EjecutarProceso(Tipo){
@@ -120,18 +120,18 @@ function EjecutarProceso(Tipo){
 		var Series = document.getElementById("Series").value;
 		var DG_Actividades=document.getElementById("DG_Actividades");
 		var DG_Llamadas=document.getElementById("DG_Llamadas");
-		
+
 		$.ajax({
 			url:"ajx_ejecutar_json.php",
 			data:{type:2,Evento:Evento,FechaInicial:FechaInicial,FechaFinal:FechaFinal,Sucursal:Sucursal,Serie:Series,Tipo:Tipo},
 			dataType:'json',
 			success: function(data){
 				if(data.Estado==1){
-					$("#UltEjecucion").html(MostrarFechaHora());				
+					$("#UltEjecucion").html(MostrarFechaHora());
 					DG_Actividades.src="detalle_cierre_ot_lote_actividades.php";
-					DG_Llamadas.src="detalle_cierre_ot_lote_llamadas.php";	
+					DG_Llamadas.src="detalle_cierre_ot_lote_llamadas.php";
 					ConsultarCant();
-					$('.ibox-content').toggleClass('sk-loading',false);	
+					$('.ibox-content').toggleClass('sk-loading',false);
 				}
 				$('.ibox-content').toggleClass('sk-loading',false);
 				Swal.fire({
@@ -145,7 +145,7 @@ function EjecutarProceso(Tipo){
 				$('.ibox-content').toggleClass('sk-loading',false);
 			}
 		});
-		
+
 }
 
 function ConsultarCant(){
@@ -175,10 +175,10 @@ function ConsultarCant(){
 
 <div id="wrapper">
 
-    <?php include("includes/menu.php"); ?>
+    <?php include "includes/menu.php";?>
 
     <div id="page-wrapper" class="gray-bg">
-        <?php include("includes/menu_superior.php"); ?>
+        <?php include "includes/menu_superior.php";?>
         <!-- InstanceBeginEditable name="Contenido" -->
         <div class="row wrapper border-bottom white-bg page-heading">
                 <div class="col-sm-8">
@@ -203,7 +203,7 @@ function ConsultarCant(){
              <div class="row">
 				<div class="col-lg-12">
 			    <div class="ibox-content">
-					 <?php include("includes/spinner.php"); ?>
+					 <?php include "includes/spinner.php";?>
 				  <form action="cierre_ot_lote.php" method="get" id="formBuscar" class="form-horizontal">
 					  <div class="form-group">
 						<label class="col-xs-12"><h3 class="bg-success p-xs b-r-sm"><i class="fa fa-filter"></i> Datos para filtrar</h3></label>
@@ -212,17 +212,17 @@ function ConsultarCant(){
 							<label class="col-lg-1 control-label">Fechas</label>
 							<div class="col-lg-3">
 								<div class="input-daterange input-group" id="datepicker">
-									<input name="FechaInicial" type="text" class="input-sm form-control" id="FechaInicial" placeholder="Fecha inicial" value="<?php echo $FechaInicial;?>"/>
+									<input name="FechaInicial" type="text" class="input-sm form-control" id="FechaInicial" placeholder="Fecha inicial" value="<?php echo $FechaInicial; ?>"/>
 									<span class="input-group-addon">hasta</span>
-									<input name="FechaFinal" type="text" class="input-sm form-control" id="FechaFinal" placeholder="Fecha final" value="<?php echo $FechaFinal;?>" />
+									<input name="FechaFinal" type="text" class="input-sm form-control" id="FechaFinal" placeholder="Fecha final" value="<?php echo $FechaFinal; ?>" />
 								</div>
 							</div>
 							<label class="col-lg-1 control-label">Sucursal <span class="text-danger">*</span></label>
 							<div class="col-lg-2">
 								<select name="Sucursal" class="form-control" id="Sucursal" required>
 									<option value="">Seleccione...</option>
-								  <?php	while($row_Sucursal=sqlsrv_fetch_array($SQL_Sucursal)){?>
-											<option value="<?php echo $row_Sucursal['IdSucursal'];?>" <?php if(isset($_GET['Sucursal'])&&(strcmp($row_Sucursal['IdSucursal'],$_GET['Sucursal'])==0)){ echo "selected=\"selected\"";}?>><?php echo $row_Sucursal['DeSucursal'];?></option>
+								  <?php	while ($row_Sucursal = sqlsrv_fetch_array($SQL_Sucursal)) {?>
+											<option value="<?php echo $row_Sucursal['IdSucursal']; ?>" <?php if (isset($_GET['Sucursal']) && (strcmp($row_Sucursal['IdSucursal'], $_GET['Sucursal']) == 0)) {echo "selected=\"selected\"";}?>><?php echo $row_Sucursal['DeSucursal']; ?></option>
 									<?php }?>
 								</select>
 							</div>
@@ -230,21 +230,21 @@ function ConsultarCant(){
 							<div class="col-lg-2">
 								<select name="Series" class="form-control" id="Series" required>
 										<option value="">Seleccione...</option>
-								  <?php if($sw==1){ 
-											while($row_Series=sqlsrv_fetch_array($SQL_Series)){?>
-											<option value="<?php echo $row_Series['IdSeries'];?>" <?php if((isset($_GET['Series']))&&(strcmp($row_Series['IdSeries'],$_GET['Series'])==0)){ echo "selected=\"selected\"";}?>><?php echo $row_Series['DeSeries'];?></option>
-								  <?php 	}
-										}?>
+								  <?php if ($sw == 1) {
+    while ($row_Series = sqlsrv_fetch_array($SQL_Series)) {?>
+											<option value="<?php echo $row_Series['IdSeries']; ?>" <?php if ((isset($_GET['Series'])) && (strcmp($row_Series['IdSeries'], $_GET['Series']) == 0)) {echo "selected=\"selected\"";}?>><?php echo $row_Series['DeSeries']; ?></option>
+								  <?php }
+}?>
 								</select>
-							</div>							
+							</div>
 						</div>
 					 	<div class="form-group">
 							<label class="col-lg-1 control-label">Estado OT <span class="text-danger">*</span></label>
 							<div class="col-lg-2">
 								<select name="EstadoLlamada" class="form-control" id="EstadoLlamada" required>
 										<option value="">Seleccione...</option>
-								  <?php while($row_EstadoLlamada=sqlsrv_fetch_array($SQL_EstadoLlamada)){?>
-										<option value="<?php echo $row_EstadoLlamada['Cod_Estado'];?>" <?php if((isset($_GET['EstadoLlamada']))&&(strcmp($row_EstadoLlamada['Cod_Estado'],$_GET['EstadoLlamada'])==0)){ echo "selected=\"selected\"";}?>><?php echo $row_EstadoLlamada['NombreEstado'];?></option>
+								  <?php while ($row_EstadoLlamada = sqlsrv_fetch_array($SQL_EstadoLlamada)) {?>
+										<option value="<?php echo $row_EstadoLlamada['Cod_Estado']; ?>" <?php if ((isset($_GET['EstadoLlamada'])) && (strcmp($row_EstadoLlamada['Cod_Estado'], $_GET['EstadoLlamada']) == 0)) {echo "selected=\"selected\"";}?>><?php echo $row_EstadoLlamada['NombreEstado']; ?></option>
 								  <?php }?>
 								</select>
 							</div>
@@ -259,7 +259,7 @@ function ConsultarCant(){
 			</div>
 		  </div>
          <br>
-		<?php if($sw==1){?>
+		<?php if ($sw == 1) {?>
 		<div class="row">
 				<div class="col-lg-2">
 					<div class="ibox">
@@ -316,17 +316,17 @@ function ConsultarCant(){
 		 <div class="row">
            <div class="col-lg-12">
 			    <div class="ibox-content">
-					 <?php include("includes/spinner.php"); ?>
+					 <?php include "includes/spinner.php";?>
 					<div class="row">
 						<div class="col-lg-8">
 							<button class="btn btn-primary btn-lg btn-outline m-b-md" type="button" id="CierreLlamadas" onClick="Validar('2');"><i class="fa fa-play-circle"></i> Cerrar llamadas de servicio</button>
-							<input type="hidden" id="IdEvento" value="<?php if(isset($row['IdEvento'])){echo $row['IdEvento'];}?>" />
+							<input type="hidden" id="IdEvento" value="<?php if (isset($row['IdEvento'])) {echo $row['IdEvento'];}?>" />
 						</div>
 						<div class="col-lg-2">
 							<div class="form-group border">
 								<div class="p-xs">
 									<label class="text-muted">Última consulta</label>
-									<div class="font-bold"><?php echo date('Y-m-d H:i');?></div>
+									<div class="font-bold"><?php echo date('Y-m-d H:i'); ?></div>
 								</div>
 							</div>
 						</div>
@@ -339,47 +339,47 @@ function ConsultarCant(){
 							</div>
 						</div>
 					</div>
-					<div class="tabs-container m-b-md">  
+					<div class="tabs-container m-b-md">
 						<ul class="nav nav-tabs">
-							<li class="active"><a data-toggle="tab" href="#tab-1"><i class="fa fa-list"></i> Llamadas de servicio</a></li>							
+							<li class="active"><a data-toggle="tab" href="#tab-1"><i class="fa fa-list"></i> Llamadas de servicio</a></li>
 							<li><span class="TimeAct"><div id="TimeAct">&nbsp;</div></span></li>
 						</ul>
 						<div class="tab-content">
 							<div id="tab-1" class="tab-pane active">
 								<iframe id="DG_Llamadas" name="DG_Llamadas" style="border: 0;" width="100%" height="400" src="detalle_cierre_ot_lote_llamadas.php"></iframe>
 							</div>
-						</div>					
+						</div>
 					</div>
 					<button class="btn btn-success btn-lg btn-outline m-b-md" type="button" id="CierreAct" onClick="Validar('1');"><i class="fa fa-play-circle"></i> Cerrar actividades</button>
-					<div class="tabs-container">  
+					<div class="tabs-container">
 						<ul class="nav nav-tabs">
-							<li class="active"><a data-toggle="tab" href="#tab-2"><i class="fa fa-tasks"></i> Actividades</a></li>								
+							<li class="active"><a data-toggle="tab" href="#tab-2"><i class="fa fa-tasks"></i> Actividades</a></li>
 							<li><span class="TimeAct"><div id="TimeAct2">&nbsp;</div></span></li>
 						</ul>
 						<div class="tab-content">
 							<div id="tab-2" class="tab-pane active">
 								<iframe id="DG_Actividades" name="DG_Actividades" style="border: 0;" width="100%" height="400" src="detalle_cierre_ot_lote_actividades.php"></iframe>
 							</div>
-						</div>					
+						</div>
 					</div>
 				</div>
-			</div>			
-          </div>	
+			</div>
+          </div>
 		 <?php }?>
         </div>
         <!-- InstanceEndEditable -->
-        <?php include("includes/footer.php"); ?>
+        <?php include "includes/footer.php";?>
 
     </div>
 </div>
-<?php include("includes/pie.php"); ?>
+<?php include "includes/pie.php";?>
 <!-- InstanceBeginEditable name="EditRegion4" -->
  <script>
         $(document).ready(function(){
 			$("#formBuscar").validate({
 			 submitHandler: function(form){
 				var reload = document.getElementById("reload");
-				 <?php if($sw==1){?>
+				 <?php if ($sw == 1) {?>
 				 	Swal.fire({
 						title: "Cierre de OT",
 						text: "¿Deseas volver a consultar este cierre o generar uno nuevo?",
@@ -396,7 +396,7 @@ function ConsultarCant(){
 						$('.ibox-content').toggleClass('sk-loading');
 				 		form.submit();
 					});
-			 	 <?php }else{?>
+			 	 <?php } else {?>
 				 	$('.ibox-content').toggleClass('sk-loading');
 				 	form.submit();
 			 	 <?php }?>
@@ -422,10 +422,10 @@ function ConsultarCant(){
 				autoclose: true,
 				format: 'yyyy-mm-dd',
 				todayHighlight: true
-			}); 
-			
+			});
+
 			$('.chosen-select').chosen({width: "100%"});
-			
+
 			var options = {
 				url: function(phrase) {
 					return "ajx_buscar_datos_json.php?type=7&id="+phrase;
@@ -445,11 +445,11 @@ function ConsultarCant(){
 			};
 
 			$("#NombreClienteActividad").easyAutocomplete(options);
-			
-			<?php if($sw==1){?>
+
+			<?php if ($sw == 1) {?>
 			ConsultarCant();
 			<?php }?>
-			
+
             $('.dataTables-example').DataTable({
                 pageLength: 25,
                 dom: '<"html5buttons"B>lTfgitp',
